a61667a6402bf592e6d0e03d30fcc712be7afd66
polinasydorenko <uvarova.pl@gmail.com>
2024-03-27 21:12:15 +0200

    Homework1

--- src/homework2/homework2task2.java
+++ src/homework2/homework2task2.java
@@ -14,28 +14,25 @@ public class homework2task2 {
 
         String[] words = text.split("\\s+");
 
-        int lengthWords = words.length;
-
-        int o = words[0].length(), a = words[1].length(),
-                b = words[2].length(), c = words[3].length(), d = words[4].length();
-
-        if (o > a){
-
-
-
-
-        }
-
-
-
-        //boolean result = words[0].length() > words[1; 2;].length;
-
+        //int lengthWords = words.length; - определение количества слов в предложении для дальнейшего сравнения.
 
         System.out.println("The first word is \"" + words[0].replace(",", "")
                 + "\" and it has " + words[0].length() + " symbols");
         System.out.println("The second word is \"" + words[1]
                 + "\" and it has " + words[1].length() + " symbols");
         System.out.println();
+
+        int w1 = words[0].length(), w2 = words[1].length(),
+                w3 = words[2].length(), w4 = words[3].length(), w5 = words[4].length();
+
+        if (w1 > w2 && w1 > w3 && w1 > w4 && w1 > w5)
+        {System.out.println("The first word is longet than other and it is \nTRUE");
+        } else {
+            System.out.println("Saying the first word is longer than others is \nFALSE");
+
+
+        }
+
 
 
     }
